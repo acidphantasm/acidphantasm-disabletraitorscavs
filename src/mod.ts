974eba0d0fb0925d9b509915fc67224ae8a47db6
acidphantasm <127812106+acidphantasm@users.noreply.github.com>
2024-06-09 15:17:35 -0500

    Added check for mod being overwritten

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,9 @@
 import { DependencyContainer, container } from "tsyringe";
 
 import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { PreAkiModLoader } from "@spt-aki/loaders/PreAkiModLoader";
 import { ConfigServer } from "@spt-aki/servers/ConfigServer";
 import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
 import { IInRaidConfig } from "@spt-aki/models/spt/config/IInRaidConfig";
@@ -9,7 +11,7 @@ import { VFS } from "@spt-aki/utils/VFS";
 import { jsonc } from "jsonc";
 import path from "node:path";
 
-class DisableTraitorScavs implements IPreAkiLoadMod
+class DisableTraitorScavs implements IPreAkiLoadMod, IPostDBLoadMod
 {
     private mod: string
     private logger: ILogger
@@ -21,11 +23,10 @@ class DisableTraitorScavs implements IPreAkiLoadMod
     {
         this.mod = "acidphantasm-disabletraitorscavs"; // Set name of mod so we can log it to console later
     }
-
     public preAkiLoad(container: DependencyContainer): void
     {
         this.logger = container.resolve<ILogger>("WinstonLogger");
-        const configServer = container.resolve<ConfigServer>("ConfigServer");
+        const configServer = container.resolve<ConfigServer>("ConfigServer"); 
         const inRaidConfig: IInRaidConfig = configServer.getConfig<IInRaidConfig>(ConfigTypes.IN_RAID);
         let localChance = DisableTraitorScavs.config.traitorScavChance;
 
@@ -53,6 +54,18 @@ class DisableTraitorScavs implements IPreAkiLoadMod
                 inRaidConfig.playerScavHostileChancePercent = localChance;
                 this.logger.log(`[${this.mod}] Traitor Scav Chance: ${inRaidConfig.playerScavHostileChancePercent}%`, "cyan");
             }
+        }
+    }
+    
+    postDBLoad(container: DependencyContainer): void 
+    {
+        this.logger = container.resolve<ILogger>("WinstonLogger");        
+        const configServer = container.resolve<ConfigServer>("ConfigServer"); 
+        const inRaidConfig: IInRaidConfig = configServer.getConfig<IInRaidConfig>(ConfigTypes.IN_RAID);
+
+        if (inRaidConfig.playerScavHostileChancePercent !== DisableTraitorScavs.config.traitorScavChance && !DisableTraitorScavs.config.disableTraitorScavs)
+        {
+            this.logger.error(`[${this.mod}] is being overwritten by another mod. Adjust your load order to have this mod load last.`)
         }
     }
 }
